135b61ab937a5b5a157ecabcb9aa571349004637
Zytekaron <zytekaron@gmail.com>
2021-04-04 22:14:29 -0700

    update main: support badger and mongo selector, default badger with local .class storage

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/zytekaron/class
 
 go 1.15
 
-require github.com/dgraph-io/badger/v3 v3.2011.1
+require (
+	github.com/dgraph-io/badger/v3 v3.2011.1
+	github.com/spf13/pflag v1.0.3
+	go.mongodb.org/mongo-driver v1.5.1
+)
--- main.go
+++ main.go
@@ -3,36 +3,68 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/spf13/pflag"
 	"github.com/zytekaron/class/v1/cli"
-	"github.com/zytekaron/class/v1/storage"
+	"github.com/zytekaron/class/v1/db"
 	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	cli.Init("0.1.2")
+var (
+	dbType string
+	// badger
+	dir string
+	// mongo
+	uri     string
+	dbName  string
+	colName string
+)
 
-	err := storage.Open()
-	if err != nil {
+func init() {
+	pflag.StringVarP(&dbType, "type", "t", "badger", "The type of database to use (badger, mongodb)")
+	pflag.StringVarP(&dir, "dir", "d", ".class", "The storage directory (badger only)")
+	pflag.StringVarP(&uri, "uri", "u", "", "The connection uri (mongodb only)")
+	pflag.StringVarP(&dbName, "dbName", "n", "", "The database name (mongodb only)")
+	pflag.StringVarP(&colName, "colName", "c", "", "The collection name (mongodb only)")
+	pflag.Parse()
+}
+
+func main() {
+	dbase := initDb()
+	if err := dbase.Open(); err != nil {
 		log.Fatal(err)
 	}
-	defer func() {
-		err := storage.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	defer dbase.Close()
 
-	if len(os.Args) == 1 {
+	cli.Init(dbase, "0.2.0")
+
+	args := pflag.Args()
+	if len(args) == 0 {
 		repl()
 		return
 	}
-
-	err = run(os.Args[1:])
-	if err != nil {
+	if err := run(args); err != nil {
 		fmt.Println(err.Error())
 		os.Exit(2)
+	}
+}
+
+func initDb() db.Database {
+	switch dbType {
+	case "badger":
+		if dir == "" {
+			log.Println("dir must be provided")
+		}
+		return db.NewBadger(dir)
+	case "mongo", "mongodb":
+		if uri == "" || dbName == "" || colName == "" {
+			log.Fatal("uri, dbName, and colName must be provided")
+		}
+		return db.NewMongo(uri, dbName, colName)
+	default:
+		log.Fatal("db type must be one of: badger, mongodb")
+		return nil
 	}
 }
 
